d1293a95522d7385a5c58d92198d754c704560fb
Draihal <draihal.a@gmail.com>
2019-08-25 23:55:10 +0300

    Clean up

--- api.py
+++ api.py
@@ -10,6 +10,7 @@ class API:
     def __init__(self, templates_dir="templates"):
         self.routes = {}
         self.templates_env = Environment(loader=FileSystemLoader(os.path.abspath(templates_dir)))
+        self.exception_handler = None
 
     def __call__(self, environ, start_response):
         request = Request(environ)
@@ -40,18 +41,26 @@ class API:
     def handle_request(self, request):
         response = Response()
         handler, kwargs = self.find_handler(request_path=request.path)
-        if handler is not None:
-            if inspect.isclass(handler):
-                handler = getattr(handler(), request.method.lower(), None)
-                if handler is None:
-                    raise AttributeError("Method now allowed", request.method)
-
-            handler(request, response, **kwargs)
-        else:
-            self.default_response(response)
+        try:
+            if handler is not None:
+                if inspect.isclass(handler):
+                    handler = getattr(handler(), request.method.lower(), None)
+                    if handler is None:
+                        raise AttributeError("Method not allowed", request.method)
+                handler(request, response, **kwargs)
+            else:
+                self.default_response(response)
+        except Exception as e:
+            if self.exception_handler is None:
+                raise e
+            else:
+                self.exception_handler(request, response, e)
         return response
 
     def template(self, template_name, context=None):
         if context is None:
             context = {}
         return self.templates_env.get_template(template_name).render(**context)
+
+    def add_exception_handler(self, exception_handler):
+        self.exception_handler = exception_handler
--- app.py
+++ app.py
@@ -1,36 +1,19 @@
-# def app(environ, start_response):
-#     response_body = b"Hello, World!"
-#     status = "200 OK"
-#     start_response(status, headers=[])
-#     return iter([response_body])
-
-# pip install gunicorn
-# gunicorn app:app
-
 from api import API
 
 app = API()
 
 
-# @app.route("/home")
-# def home(request, response):
-#     response.text = "Hello from the HOME page."
-#
-#
-# @app.route("/about")
-# def about(request, response):
-#     response.text = "Hello from the ABOUT page."
-
-def handler(req, resp):
-    resp.text = "Hello from the HOME page."
-
-
-def handler2(req, resp):
+def index_page(req, resp):
     resp.text = "Hello from the INDEX page."
 
 
-app.add_route("/home", handler)
-app.add_route("/", handler2)
+def home_page(req, resp):
+    resp.text = "Hello from the HOME page."
+
+
+app.add_route("/", index_page)
+app.add_route("/home", home_page)
+app.add_route("/home/", home_page)
 
 
 @app.route("/hello/{name}")
@@ -49,10 +32,24 @@ class TestResource:
     def get(self, req, resp):
         resp.text = "Test Page"
 
+
 @app.route("/template")
+@app.route("/template/")
 def template_handler(req, resp):
     resp.body = app.template(
         "index.html",
         context={"title": "WSGI Framework", "body": "Testing template with jinja."}
     ).encode()
 
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = "Oops! Something went wrong."
+
+
+app.add_exception_handler(custom_exception_handler)
+
+
+@app.route("/except")
+@app.route("/except/")
+def exception_throwing_handler(request, response):
+    raise AssertionError("Testing exeption.")
